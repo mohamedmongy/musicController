80e25961b4fdfee3ad0d4069a80a8d3fbcc88789
mohamed.mongy <mohamed.mongy@samesystem.com>
2022-10-08 00:29:57 +0200

    set up frontend apps

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+from .serializers import RoomSerializer
+from rest_framework import generics
+# Create your views here.
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    
